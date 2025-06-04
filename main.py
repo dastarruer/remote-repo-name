ea8342941a4b42d3017b312c8be9b2d36c7aa5dc
dastarruer <ayushpramanik399@gmail.com>
2025-06-04 14:50:37 -0400

    Add texture to mesh

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ import pyvista as pv
 
 # Convert to grayscale and save as .tiff
 img = Image.open("images/satellite-image-high.png").convert('L')
-
-# Resize to manageable size (optional)
-# img = img.resize((300, 300))
+# Rotate the image so that it will show properly when put on the mesh
+img.transpose(Image.ROTATE_90).save("images/output/flipped.png")
 
 # Convert to numpy array (0-255)
 heightmap = np.array(img)
@@ -22,8 +21,12 @@ xv, yv = np.meshgrid(x, y)
 
 # Create 3D terrain mesh using pyvista
 grid = pv.StructuredGrid(xv, yv, heightmap)
+grid.texture_map_to_plane(inplace=True)
 
 # Visualize
 plotter = pv.Plotter()
-plotter.add_mesh(grid, cmap="terrain", lighting="True", label="The Gaza strip", render_points_as_spheres="True")
+texture= pv.read_texture("images/output/flipped.png")
+plotter.add_mesh(grid, cmap="terrain", lighting="True", label="The Gaza strip", render_points_as_spheres="True", texture=texture)
+
+# Save as a screenshot once closed
 plotter.show(screenshot="images/output/render.png")
