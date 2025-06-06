cedcc47b97587a47cd4c325537a55088722e6c8e
dastarruer <ayushpramanik399@gmail.com>
2025-06-05 22:07:46 -0400

    Add cli arg to point to an image to convert to obj

--- main.py
+++ main.py
@@ -6,35 +6,38 @@ import getopt, sys
 
 
 BASE_DIR = path.dirname(__file__)
-TOPOGRAPHY_IMG = path.join(BASE_DIR, "images", "topography-image.png")
-MODIFIED_TOPOGRAPHY_IMG = path.join(BASE_DIR, "images", "modified-image.png")
+topography_img = path.join(BASE_DIR, "images", "topography-image.png")
+MODIFIED_TOPOGRAPHY_IMG = path.join(BASE_DIR, "modified-image.png")
 FINAL_MODEL_PATH = path.join(BASE_DIR, "model", "model.obj")
 
-FLAGS = ["show"]
-OPTIONS = "s"
+FLAGS = ["show", "image="]
+OPTIONS = "si:"
 
 def main():
+    global topography_img
+    
     argument_list = sys.argv[1:]
-    arguments, values = getopt.getopt(argument_list, OPTIONS, FLAGS)
+    arguments, _ = getopt.getopt(argument_list, OPTIONS, FLAGS)
 
     show = False
     for currentArgument, currentValue in arguments:
         if currentArgument in ("-s", "--show"):
             show = True
+        if currentArgument in ("-i", "--image") and path.exists(currentValue):
+            topography_img = path.abspath(currentValue)
             
     img = process_image()
-    plotter = create_model(img, show=show)
+    create_model(img, show=show)
 
-    # Export the model
-    plotter.export_obj(FINAL_MODEL_PATH)
+    
     
 
 def process_image() -> Image.Image:
     # Reset modified image
-    Image.open(TOPOGRAPHY_IMG).save(MODIFIED_TOPOGRAPHY_IMG)
+    Image.open(topography_img).save(MODIFIED_TOPOGRAPHY_IMG)
 
     # Convert to grayscale, where brighter values will show as peaks, and darker values will show as lows
-    img = Image.open(TOPOGRAPHY_IMG).convert('L')
+    img = Image.open(topography_img).convert('L')
     
     # Downscale the image so the program doesn't crash all the time
     scale_factor = 1
@@ -48,7 +51,7 @@ def process_image() -> Image.Image:
     return img
 
 
-def create_model(img, show=False) -> pv.Plotter:
+def create_model(img, show=False) -> None:
     # Convert to numpy array (0-255)
     heightmap = np.array(img)
 
@@ -67,8 +70,9 @@ def create_model(img, show=False) -> pv.Plotter:
     grid.texture_map_to_plane(inplace=True)
 
     # For whatever reason the image will not align with the mesh, meaning that we have to rotate it
-    Image.open(TOPOGRAPHY_IMG).transpose(Image.Transpose.ROTATE_90).save(MODIFIED_TOPOGRAPHY_IMG)
+    Image.open(topography_img).transpose(Image.Transpose.ROTATE_90).save(MODIFIED_TOPOGRAPHY_IMG)
     
+    # TODO: Fix issue where text will be flipped after rotating texture 90 degrees
     # Create the texture
     texture= pv.read_texture(MODIFIED_TOPOGRAPHY_IMG)
 
@@ -77,7 +81,9 @@ def create_model(img, show=False) -> pv.Plotter:
 
     if show == True:
         plotter.show()
-    return plotter
+
+    # Export the model
+    plotter.export_obj(FINAL_MODEL_PATH)
 
         
 
